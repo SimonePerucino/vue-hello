9ee0744890a52498c0584fd1d0192c9b2536779a
SimonePerucino <simone.perucino@gmail.com>
2021-05-03 20:23:41 +0200

    Esercizio Finito

---
+++ javascript.js
@@ -0,0 +1,26 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            messaggio: "Boolean",
+            img:"https://media-exp1.licdn.com/dms/image/C4D0BAQErbNlWeMNGDQ/company-logo_200_200/0/1600963347198?e=2159024400&v=beta&t=LkP_rNTixZIzvAmMyh0YGpXxVYb5nW9R6-VuFWJS3ck"
+        },
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
